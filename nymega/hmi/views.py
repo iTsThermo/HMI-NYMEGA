3850acff200b4ab00188d13e5245bf35543605aa
iTsThermo <117327847+iTsThermo@users.noreply.github.com.>
2025-07-19 20:34:48 -0400

    added templates and hmi html

---
+++ nymega/hmi/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
